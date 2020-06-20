812ea621d9d94f53e8b686a1a4de07382513e6b9
João <jsoareez@gmail.com>
2020-06-20 15:42:49 -0300

    UI clone discord

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    };
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="João Vitor"
+            date="20/06/2020"
+            content="Hoje é o meu aniversário!"
+          />
+        ))}
+
+        <ChannelMessage 
+          author="Pedro Maia"
+          date="20/06/2020"
+          content={
+            <>
+              <Mention>@João Vitor</Mention>, Ai sim meu guerreiro!
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { Container, Role, User, Avatar } from './styles';
+
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+};
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+  return (
+    <User>
+      <Avatar className={isBot ? 'bot' : ''} />
+
+      <strong>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+};
+
+const UserList: React.FC = () => {
+  return (
+    <Container>
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="João Vitor" />
+      <UserRow nickname="Pedro Maia" isBot />
+
+      <Role>Indisponível - 12</Role>
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+      <UserRow nickname="Fulanos" />
+    </Container>
+  );
+};
+
+export default UserList;
